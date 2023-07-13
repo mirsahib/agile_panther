a2361ac4e3c0b568ca0ff7e024bc7e0bf2e8a5ea
Mir Sahib <mirsahib24@gmail.com>
2023-07-13 16:42:40 +0600

    :hammer: Add shadcn base-color:slate-default

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import { Button } from './components/ui/button'
 function App() {
   
   return (
     <>
       <div className='text-3xl font-bold underline'> Hello world</div>
+      <Button variant={"default"}>Default Button</Button>
+      <Button variant={"secondary"}>Secondary Button</Button>
+      <Button variant={"outline"}>Outline Button</Button>
+      <Button variant={"destructive"}>Destructive Button</Button>
+      <Button variant={"link"}>Link Button</Button>
+      <Button variant={"ghost"}>Ghost Button</Button>
+
     </>
   )
 }
